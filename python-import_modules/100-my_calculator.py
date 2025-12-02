a5d464c6e79446b1e6222b1eb0cbdb9683589e82
k7nain <murslizadkenan@gmail.com>
2025-12-02 14:07:17 +0000

    Python - import & modules Advanced Task 6

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
 import sys
+
 if __name__ == "__main__":
-    num_args = len(sys.argv) - 1
-    if num_args != 3:
+    if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    a = int((sys.argv[1]))
+
+    a = int(sys.argv[1])
     operator = sys.argv[2]
     b = int(sys.argv[3])
 
