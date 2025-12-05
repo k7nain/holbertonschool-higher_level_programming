3d80776af1578c5f2dc074e9b7169b1e0eb0d336
k7nain <murslizadkenan@gmail.com>
2025-12-05 05:46:26 +0000

    Python - Data Structures: Lists, Tuples Task 4

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     new_list = []
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return my_list
     for i in my_list:
         new_list.append(i)
