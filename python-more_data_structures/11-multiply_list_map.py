682a26048cf1007aa3ceb9373e16ecc0ed58a6d5
k7nain <murslizadkenan@gmail.com>
2025-12-11 05:48:53 +0000

    Python - More Data Structures: Set, Dictionary Task 11

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    new_list = []
-    for num in my_list:
-        num = num * number
-        new_list.append(num)
-    return new_list
+    return list(map(lambda num: num * number, my_list))
