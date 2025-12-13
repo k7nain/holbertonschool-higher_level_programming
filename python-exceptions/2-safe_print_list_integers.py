617fd9609a04a6b69fe6df9c99ae347be51c0223
k7nain <murslizadkenan@gmail.com>
2025-12-13 19:40:10 +0000

    Python - Exceptions Task 2

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,16 @@
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except (ValueError, TypeError):
+            # integer deyil → ötür
+            pass
+        except IndexError:
+            # siyahı bitdi → çıx
+            break
+
+    print()
+    return count
