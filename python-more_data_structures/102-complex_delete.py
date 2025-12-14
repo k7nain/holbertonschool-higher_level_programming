0110fe989b053362c1e726a5e2c987209f9d0bc9
k7nain <murslizadkenan@gmail.com>
2025-12-14 13:43:34 +0000

    Python - More Data Structures: Set, Dictionary Advanced Task 15

--- python-more_data_structures/102-complex_delete.py
+++ python-more_data_structures/102-complex_delete.py
@@ -2,6 +2,6 @@
 def complex_delete(a_dictionary, value):
     keys = list(a_dictionary.keys())
     for key in keys:
-        if a_dictionary[key] == "C":
+        if a_dictionary[key] == value:
             del a_dictionary[key]
     return a_dictionary
