b310c1dfebc78737d17e67ccef1dcf0fe953968d
k7nain <murslizadkenan@gmail.com>
2025-12-17 20:42:23 +0000

    Python - Classes and Objects Task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module defines a Square class with a private size attribute."""
+
+
+class Square:
+    """This class defines a square with a private size."""
+    def __init__(self, size):
+        self.__size = size
