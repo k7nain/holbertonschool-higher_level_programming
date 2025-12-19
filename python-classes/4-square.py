e915e1adc7d1fa17b7a6410ddcbf26112c63c2de
k7nain <murslizadkenan@gmail.com>
2025-12-19 09:02:31 +0000

    Python - Classes and Objects Task 4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+"""
+This module defines a Square class with a private size attribute,
+including getter and setter methods with validation.
+"""
 
 
 class Square:
     """
-    Square class that defines a square with a private size attribute
+    Square class that defines a square by its size
     """
 
     def __init__(self, size=0):
@@ -28,7 +32,6 @@ class Square:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
-
         self.__size = value
 
     def area(self):
