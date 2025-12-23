868a5001c8e948571f23426c6fdee398f5f9d37c
k7nain <murslizadkenan@gmail.com>
2025-12-23 19:32:49 +0000

    Python - Inheritance Task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a function that checks object class."""
+
+
+class BaseGeometry:
+
+    """ Class with Exception"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
