5eada16edd4e21f4205ae3964ca73ab7114f47ba
k7nain <murslizadkenan@gmail.com>
2025-12-26 19:46:03 +0000

    Python - Inheritance Task 9

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module that defines Rectangle class"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+
+    """Define Rectangle class"""
+
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+    def area(self):
+        return self.__height * self.__width
