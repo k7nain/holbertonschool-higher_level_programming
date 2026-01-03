06574f9efb983c017143cc70ba7cae6a7ca977d4
k7nain <murslizadkenan@gmail.com>
2026-01-03 18:00:51 +0000

    Python - Input/Output Task 10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Defines a Student class."""
+
+
+class Student:
+    """Represents a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return a dictionary representation of the Student."""
+        if isinstance(attrs, list):
+            result = {}
+            for attr in attrs:
+                if hasattr(self, attr):
+                    result[attr] = getattr(self, attr)
+            return result
+        return self.__dict__
