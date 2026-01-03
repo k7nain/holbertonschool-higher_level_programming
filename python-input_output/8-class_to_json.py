cd94ae50de096cfb990e90956dc63f83cbc731ac
k7nain <murslizadkenan@gmail.com>
2026-01-03 17:06:54 +0000

    Python - Input/Output Task 8

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Returns a dictionary description of an object for JSON serialization."""
 
+
 def class_to_json(obj):
     """Return the dictionary representation of an object."""
     return obj.__dict__
