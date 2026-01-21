22e374d040b0efb2039f1fbe9c1ecf232679e3cf
k7nain <murslizadkenan@gmail.com>
2026-01-21 21:29:26 +0000

    SQL - More queries Task 9

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- JOIN
-SELECT cities.id, cities.name AS city_name, states.name AS state_name
+SELECT cities.id, cities.name 
 FROM cities
 JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
