1f169e7603d058390f1276dedcb7b328a98d0926
k7nain <murslizadkenan@gmail.com>
2026-02-20 22:21:55 +0000

    JavaScript - Warm up Task 10

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 
-function factorial(n) {
-    if (isNaN(n) || n <= 1) {
-        return 1;
-    }
-    return n * factorial(n - 1);
+const num = parseInt(process.argv[2]);
+
+function factorial (n) {
+  if (isNaN(n) || n <= 0) {
+    return 1;
+  }
+  return n * factorial(n - 1);
 }
-
-const n = parseInt(process.argv[2]);
-
-console.log(factorial(n));
+console.log(factorial(num));
