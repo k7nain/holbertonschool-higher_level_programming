903c73f5d823186280237b77a58248464d4a76ed
k7nain <murslizadkenan@gmail.com>
2026-02-23 14:02:15 +0000

    JavaScript - Warm up Task 12

--- javascript-warm_up/12-object.js
+++ javascript-warm_up/12-object.js
@@ -2,9 +2,10 @@
 const myObject = {
     type: 'object',
     value: 12
-};
-console.log(myObject);
-
-myObject['value'] = 89;
-
-console.log(myObject);
+  };
+  console.log(myObject);
+  
+  myObject.value = 89;
+  
+  console.log(myObject);
+  
