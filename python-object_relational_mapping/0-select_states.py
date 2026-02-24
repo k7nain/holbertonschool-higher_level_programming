69b0944809f63a7afdd7103813ae1f642b222ed5
k7nain <murslizadkenan@gmail.com>
2026-02-24 20:10:50 +0000

    Python - Object-relational mapping Task

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 """
-First ORM
+Lists all states from the database hbtn_0e_0_usa
 """
 
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
+    # Al arguments: username, password, database
     user = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
+    # Connect to MySQL on localhost:3306
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -20,8 +22,10 @@ if __name__ == "__main__":
     )
 
     cursor = db.cursor()
+    # Execute query, order by id ascending
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
+    # Print each row
     for row in cursor.fetchall():
         print(row)
 
