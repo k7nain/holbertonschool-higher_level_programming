a57ec97460e0e1a5bf5f256ecbd37adb009b641f
k7nain <murslizadkenan@gmail.com>
2026-02-28 12:22:16 +0000

    JavaScript - Warm up Task 6

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
 const languages = [
-    'C is fun',
-    'Python is cool',
-    'JavaScript is amazing'
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
 ];
 
 for (let i = 0; i < languages.length; i++) {
-    console.log(languages[i]);
+  console.log(languages[i]);
 }
