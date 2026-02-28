2da32760293aa980322c8dd98d49604d44c4015c
k7nain <murslizadkenan@gmail.com>
2026-02-28 12:27:47 +0000

    JavaScript - Warm up Task 8

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -2,10 +2,10 @@
 
 const num = parseInt(process.argv[2]);
 
-if(isNaN(num)){
-    console.log('Missing size');
+if (isNaN(num)) {
+  console.log('Missing size');
 } else {
-    for (let i = 0; i < num; i++) {
-      console.log('X'.repeat(num));
-    }
+  for (let i = 0; i < num; i++) {
+    console.log('X'.repeat(num));
+  }
 }
